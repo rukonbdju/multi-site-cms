bd71ba0c557b0292d89b893f255fcaff2c4d43da
unknown <you@example.com>
2025-10-22 14:42:55 +0600

    /me route added

--- backend/src/modules/user/user.routes.ts
+++ backend/src/modules/user/user.routes.ts
@@ -5,11 +5,12 @@ const userRouter = Router();
 const userController = new UserController();
 
 // Public routes
-userRouter.post('/', (req, res, next) => userController.register(req, res, next));
 userRouter.post('/login', (req, res, next) => userController.login(req, res, next));
 
 // Protected / Admin routes (you can add auth middleware later)
+userRouter.post('/', (req, res, next) => userController.register(req, res, next));
 userRouter.get('/', (req, res, next) => userController.listUsers(req, res, next));
+userRouter.get('/me', (req, res, next) => userController.getUserById(req, res, next));
 userRouter.get('/:id', (req, res, next) => userController.getUserById(req, res, next));
 userRouter.put('/:id', (req, res, next) => userController.updateUser(req, res, next));
 userRouter.delete('/:id', (req, res, next) => userController.deleteUser(req, res, next));
